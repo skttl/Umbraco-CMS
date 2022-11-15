c9f479eb938c918a828d31e56aa6e801479c9568
Søren Kottal <sk@ecreo.dk>
2022-11-15 23:23:40 +0100

    Fix merge error

--- src/Umbraco.Infrastructure/PropertyEditors/ValueConverters/BlockListPropertyValueConverter.cs
+++ src/Umbraco.Infrastructure/PropertyEditors/ValueConverters/BlockListPropertyValueConverter.cs
@@ -1,14 +1,8 @@
 // Copyright (c) Umbraco.
 // See LICENSE for more details.
 
-using Umbraco.Cms.Core.Logging;
-using Umbraco.Cms.Core.Models;
 using Umbraco.Cms.Core.Models.Blocks;
-using Umbraco.Cms.Core.Models.PublishedContent;
-using Umbraco.Cms.Core.Services;
-using Umbraco.Cms.Web.Common.DependencyInjection;
 using Umbraco.Extensions;
-using static Umbraco.Cms.Core.PropertyEditors.BlockListConfiguration;
 
 namespace Umbraco.Cms.Core.PropertyEditors.ValueConverters;
 
@@ -86,29 +80,6 @@ public class BlockListPropertyValueConverter : BlockPropertyValueConverterBase<B
         using (_proflog.DebugDuration<BlockListPropertyValueConverter>(
                    $"ConvertPropertyToBlockList ({propertyType.DataType.Id})"))
         {
-            var value = (string?)inter;
-
-            var defaultValue = IsSingleBlockMode(propertyType.DataType) ? null : BlockListModel.Empty;
-
-            // Short-circuit on empty values
-            if (string.IsNullOrWhiteSpace(value))
-            {
-                return defaultValue;
-            }
-
-            BlockEditorData converted = _blockListEditorDataConverter.Deserialize(value);
-            if (converted.BlockValue.ContentData.Count == 0)
-            {
-                return defaultValue;
-            }
-
-            IEnumerable<BlockListLayoutItem>? blockListLayout =
-                converted.Layout?.ToObject<IEnumerable<BlockListLayoutItem>>();
-            if (blockListLayout is null)
-            {
-                return defaultValue;
-            }
-
             // Get configuration
             BlockListConfiguration? configuration = propertyType.DataType.ConfigurationAs<BlockListConfiguration>();
             if (configuration is null)
@@ -122,80 +93,7 @@ public class BlockListPropertyValueConverter : BlockPropertyValueConverterBase<B
 
             BlockListModel blockModel = UnwrapBlockModel(referenceCacheLevel, inter, preview, configuration.Blocks, CreateEmptyModel, CreateModel);
 
-            return blockModel;
-                contentPublishedElements[element.Key] = element;
-            }
-
-            // If there are no content elements, it doesn't matter what is stored in layout
-            if (contentPublishedElements.Count == 0)
-            {
-                return defaultValue;
-            }
-
-            // Convert the settings data
-            var settingsPublishedElements = new Dictionary<Guid, IPublishedElement>();
-            var validSettingsElementTypes = blockConfigMap.Values.Select(x => x.SettingsElementTypeKey)
-                .Where(x => x.HasValue).Distinct().ToList();
-            foreach (BlockItemData data in converted.BlockValue.SettingsData)
-            {
-                if (!validSettingsElementTypes.Contains(data.ContentTypeKey))
-                {
-                    continue;
-                }
-
-                IPublishedElement? element = _blockConverter.ConvertToElement(data, referenceCacheLevel, preview);
-                if (element is null)
-                {
-                    continue;
-                }
-
-                settingsPublishedElements[element.Key] = element;
-            }
-
-            // Cache constructors locally (it's tied to the current IPublishedSnapshot and IPublishedModelFactory)
-            var blockListItemActivator = new BlockListItemActivator(_blockConverter);
-
-            var list = new List<BlockListItem>();
-            foreach (BlockListLayoutItem layoutItem in blockListLayout)
-            {
-                // Get the content reference
-                var contentGuidUdi = (GuidUdi?)layoutItem.ContentUdi;
-                if (contentGuidUdi is null ||
-                    !contentPublishedElements.TryGetValue(contentGuidUdi.Guid, out IPublishedElement? contentData))
-                {
-                    continue;
-                }
-
-                if (!blockConfigMap.TryGetValue(
-                    contentData.ContentType.Key,
-                    out BlockListConfiguration.BlockConfiguration? blockConfig))
-                {
-                    continue;
-                }
-
-                // Get the setting reference
-                IPublishedElement? settingsData = null;
-                var settingGuidUdi = (GuidUdi?)layoutItem.SettingsUdi;
-                if (settingGuidUdi is not null)
-                {
-                    settingsPublishedElements.TryGetValue(settingGuidUdi.Guid, out settingsData);
-                }
-
-                // This can happen if they have a settings type, save content, remove the settings type, and display the front-end page before saving the content again
-                // We also ensure that the content types match, since maybe the settings type has been changed after this has been persisted
-                if (settingsData is not null && (!blockConfig.SettingsElementTypeKey.HasValue ||
-                                                 settingsData.ContentType.Key != blockConfig.SettingsElementTypeKey))
-                {
-                    settingsData = null;
-                }
-
-                // Create instance (use content/settings type from configuration)
-                BlockListItem layoutRef = blockListItemActivator.CreateInstance(blockConfig.ContentElementTypeKey, blockConfig.SettingsElementTypeKey, contentGuidUdi, contentData, settingGuidUdi, settingsData);
-
-                list.Add(layoutRef);
-            }
-
-            return IsSingleBlockMode(propertyType.DataType) ? list.FirstOrDefault() : new BlockListModel(list);
+            return IsSingleBlockMode(propertyType.DataType) ? blockModel.FirstOrDefault() : blockModel;
         }
     }
 
